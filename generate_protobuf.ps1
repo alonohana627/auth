e8a2c1625e45549953601b78945934b733f464f5
Alon Ohana <82659761+alonohana627@users.noreply.github.com>
2023-09-25 17:43:18 +0300

    Update generate_protobuf.ps1

--- generate_protobuf.ps1
+++ generate_protobuf.ps1
@@ -1,3 +1,3 @@
-protoc --proto_path=./pb --go_out=./pb ./pb/user.proto
-protoc --proto_path=./pb --go_out=./pb ./pb/routes.proto
-protoc --proto_path=./pb --go-grpc_out=./pb ./pb/routes.proto
+protoc --proto_path=./pb --go_out=./pb ./pb/user.proto --experimental_allow_proto3_optional
+protoc --proto_path=./pb --go_out=./pb ./pb/routes.proto --experimental_allow_proto3_optional
+protoc --proto_path=./pb --go-grpc_out=./pb ./pb/routes.proto --experimental_allow_proto3_optional
