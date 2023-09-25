d02b74d57b38ad7aecf74cb9f06bef7c346058e1
Alon Ohana <alon4dota2@gmail.com>
2023-09-25 20:00:51 +0300

    Fixed the shell script
    In the future I will remove the docker-compose (or leave for reference I guess)

--- generate_protobuf.sh
+++ generate_protobuf.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-protoc --proto_path=/AuthService/pb --go_out=/AuthService/pb /AuthService/pb/user.proto
-protoc --proto_path=/AuthService/pb --go_out=/AuthService/pb /AuthService/pb/routes.proto
-protoc --proto_path=/AuthService/pb --go-grpc_out=/AuthService/pb /AuthService/pb/routes.proto
+protoc --proto_path=/AuthService/pb --go_out=/AuthService/pb /AuthService/pb/user.proto --experimental_allow_proto3_optional
+protoc --proto_path=/AuthService/pb --go_out=/AuthService/pb /AuthService/pb/routes.proto --experimental_allow_proto3_optional
+protoc --proto_path=/AuthService/pb --go-grpc_out=/AuthService/pb /AuthService/pb/routes.proto --experimental_allow_proto3_optional
